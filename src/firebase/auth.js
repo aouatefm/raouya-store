decb4a10d74a49ab173a3d89df3666fa5e4bfa46
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-01-17 20:27:41 +0100

    Implemented firebase user auth

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,11 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import {createUserDocument} from "./user";
 
-export const signup = async (email, password) => {
-    const resp = await firebase.auth().createUserWithEmailAndPassword(email, password)
-    const user = resp.user
-    //await user.updateProfile({ displayName: `${firstName} ${lastName}` })
-    return user
+export const signup = async (email, password,firstName,lastName,phone,address) => {
+    const resp = await firebase.auth().createUserWithEmailAndPassword(email,password)
+    const userProfile = {email:email,
+                        address : address,
+                        role : 'user',
+                        displayName: `${firstName} ${lastName}`,
+                        phoneNumber :phone}
+    // const user = resp.user
+     await createUserDocument(userProfile,resp.user.uid);
+    // return user
 }
 export const logout = () => {
     return firebase.auth().signOut();
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import 'firebase/firestore';
+import 'firebase/storage';
 
 firebase.initializeApp({
     apiKey: "AIzaSyAcUtjlHUg_T120LWLpjPo2LaENn5WGYTY",
@@ -9,3 +11,9 @@ firebase.initializeApp({
     appId: "1:573274210542:web:e4e0acfc843cffc2c773ff"
 })
 console.log(firebase.app().options)
+
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase
