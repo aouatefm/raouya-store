7580dd3534915ddea5bb37a52af6b62d5b21ea3e
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-01-17 20:18:56 +0100

    Added the user role to the header component.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { Link, useHistory } from "react-router-dom";
 import Layout from '../Layout'
 import './header.scss'
 import { useSession } from '../../firebase/UserProvider'
 import { logout } from '../../firebase/auth'
+import {CartContext} from "../contexts/CartContext";
+import {CartIcon} from "../../assets/icons";
+import {getUserById} from "../../firebase/user";
 
 const Header = () => {
+    const [userRole,setUserRole] = useState()
+    const {itemCount} = useContext(CartContext);
     const history = useHistory()
     const { user } = useSession()
     const logoutUser = async () => {
         await logout();
         history.push('/');
     };
+    useEffect(async () => {
+        if(user)
+        {const role = await getUserById(user.uid);
+            await setUserRole(role.data().role)}
+
+    }, []);
 
     return (
         <>
             <header className="header" style={{ marginBottom: "50px" }}>
                 <Link to='/' className="link">Store</Link>
                 <Link to='/about' className="link">About</Link>
-                <Link to='/cart' className="link">  Cart (3)</Link>
-                {user && <Link to='/dashboard' className="link">Dashboard</Link>}
-                {user && <Link className="link" onClick={logoutUser}>Logout</Link>}
+                <Link to='/cart' className="link"> <CartIcon width={18}/> Cart ({itemCount})</Link>
+                {user && userRole==='admin' && <Link to='/dashboard' className="link">Dashboard</Link>}
                 {!user && <Link to='/login' className="link">Login</Link>}
-                  </header>
+                {user && userRole==='admin' && <Link to='/orders' className="link" >Order</Link>}
+                {user && userRole==='admin' &&<Link to='' className="link" >Hello {userRole} !</Link>}
+                {user && <Link className="link" onClick={logoutUser} to=''>Logout</Link>}
+
+            </header>
             <Layout title="Raouiya Store" description="This is the Store Page" />
 
         </>
