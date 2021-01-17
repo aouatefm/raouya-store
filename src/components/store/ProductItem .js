19c5d6c07764e8acea8ce56e9944e9b2c8bacaa5
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-01-17 20:21:07 +0100

    implemented add to cart button

--- src/components/store/ProductItem .js
+++ src/components/store/ProductItem .js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
 import { formatNumber } from '../../helpers/utils';
+import {CartContext} from "../contexts/CartContext";
 const ProductItem = ({ product }) => {
+    const { addProduct, cartItems, increase } = useContext(CartContext);
+    const isInCart = product => {
+        return !!cartItems.find(item => item.id === product.id);
+    }
     return (
-
         <div className="card card-body" >
             <img style={{ display: "block", margin: "0 auto 10px", maxHeight: "300px" }} className="img-fluid"
                 src={product.images + '?v=' + product.id} alt="" />
@@ -11,10 +15,18 @@ const ProductItem = ({ product }) => {
             <h3 className="text-left">{formatNumber(product.price)}</h3>
             <div className="text-right">
                 <Link to={{ pathname: `/products/${product.id}`, prod: product }} className="btn btn-link btn-sm mr-2" >Details</Link>
-                {/* updaaaaaaaaaaaaaate *************
-                <button className="btn btn-outline-primary btn-sm">Add more</button> */}
-                <button
-                    className="btn btn-primary btn-sm">Add to cart</button>
+                {
+                    isInCart(product) &&
+                    <button
+                        onClick={() => increase(product)}
+                        className="btn btn-outline-dark btn-sm">Add more</button>
+                }
+                {
+                    !isInCart(product) &&
+                    <button
+                        onClick={() => addProduct(product)}
+                        className="btn btn-primary btn-sm">Add to cart</button>
+                }
             </div>
         </div >
 
