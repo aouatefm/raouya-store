4fea8d30819ca32ba757bcec4106ee413b79dd52
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-01-27 21:08:11 +0100

    edited the signup method

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -2,15 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import {createUserDocument} from "./user";
 
-export const signup = async (email, password,firstName,lastName,phone,address) => {
+export const signup = async (email,password,firstName,lastName,phone,address) => {
     const resp = await firebase.auth().createUserWithEmailAndPassword(email,password)
     const userProfile = {email:email,
-                        address : address,
-                        role : 'user',
-                        displayName: `${firstName} ${lastName}`,
-                        phoneNumber :phone}
-    // const user = resp.user
-     await createUserDocument(userProfile,resp.user.uid);
+                         displayName: `${firstName} ${lastName}`,
+                         address : address,
+                         role : 'user',
+                         phoneNumber : phone,
+                        }
+    const user = resp.user
+    await user.updateProfile({ displayName: `${firstName} ${lastName}` , phoneNumber : phone})
+    await createUserDocument(userProfile,resp.user.uid);
     // return user
 }
 export const logout = () => {
